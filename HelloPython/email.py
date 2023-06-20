8d43fb142abd97b5606a46c6fedc8d3103ff6024
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2023-06-20 12:14:24 +0100

    exporing data use pandas

---
+++ HelloPython/email.py
@@ -0,0 +1,4 @@
+import re
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+# At least 8 char long
+# 
---
+++ HelloPython/regular.py
@@ -0,0 +1,12 @@
+# import re
+# pattern = re.compile(r"[a-zA-Z].[(a)]")
+# string = "search inside of this text string"
+# target = re.search('this',string)
+# # start(), end(), group()
+# a = pattern.search(string)
+# b = pattern.findall(string)
+# c = pattern.match(string)
+
+
+# print(a.group())
+
